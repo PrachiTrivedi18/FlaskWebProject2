1e10edd879085d6e699918ee1f53ac8db25c15da
Prachi Trivedi <prachisv.trivedi@yahoo.com>
2021-08-08 12:40:19 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,46 @@
+"""
+Routes and views for the flask application.
+"""
+from FlaskWebProject2 import app
+from datetime import datetime
+from azure.storage.blob import generate_container_sas, ContainerSasPermissions
+from azure.storage.blob import generate_blob_sas, BlobSasPermissions
+from flask import render_template
+
+account_name = "cdnstore1"
+account_key = "iM7sub7rvl+lBdORwaq9u5KWPiN3Q2h5sYopVxyiAbsemOkedIPeSrnmEQWeIICS32HNyznhzIO8Zol5A55BGw=="
+container_name = "images"
+
+#using generate_container_sas
+def get_img_url_with_container_sas_token(blob_name):
+    container_sas_token = generate_container_sas(
+        account_name=account_name,
+        container_name=container_name,
+        account_key=account_key,
+        permission=ContainerSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(hours=1)
+    )
+    blob_url_with_container_sas_token = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}?{container_sas_token}"
+    return blob_url_with_container_sas_token
+
+#using generate_blob_sas
+def get_img_url_with_blob_sas_token(blob_name):
+    blob_sas_token = generate_blob_sas(
+        account_name=account_name,
+        container_name=container_name,
+        blob_name=blob_name,
+        account_key=account_key,
+        permission=ContainerSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(hours=1)
+    )
+    blob_url_with_blob_sas_token = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}?{blob_sas_token}"
+    return blob_url_with_blob_sas_token
+
+@app.route('/')
+def index(blob_name="Beauty.JPEG"):
+    img_url_with_sas_token = get_img_url_with_blob_sas_token(blob_name)
+    return render_template('index.html',title='Home Page',img_url_with_sas_token=img_url_with_sas_token)
+
+
+
+

14d60b5147bb79983d2026ea4cc27535b51efe86
Prachi Trivedi <prachisv.trivedi@yahoo.com>
2021-08-10 17:07:28 +0530

    Twelve Commit

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 """
 Routes and views for the flask application.
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__, template_folder= "template")
 
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
+from azure.storage.blob import generate_container_sas, ContainerSasPermissions
 from azure.storage.blob import generate_blob_sas, BlobSasPermissions
 from flask import render_template
-
 
 account_name = "cdnstore1"
 account_key = "iM7sub7rvl+lBdORwaq9u5KWPiN3Q2h5sYopVxyiAbsemOkedIPeSrnmEQWeIICS32HNyznhzIO8Zol5A55BGw=="
 container_name = "images"
 
-#using generate_blob_sas
+# using generate_blob_sas
 def get_img_url_with_blob_sas_token(blob_name):
     blob_sas_token = generate_blob_sas(
         account_name=account_name,
@@ -27,15 +26,16 @@ def get_img_url_with_blob_sas_token(blob_name):
     blob_url_with_blob_sas_token = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}?{blob_sas_token}"
     return blob_url_with_blob_sas_token
 
+
 @app.route("/showimg")
 @app.route("/showimg/<blob_name>")
 def hello_world(blob_name=None):
     img_url_with_sas_token = get_img_url_with_blob_sas_token(blob_name)
-    return render_template('index.html',img_url_with_sas_token=img_url_with_sas_token)
+    # Or 
+    # img_url_with_sas_token = get_img_url_with_container_sas_token(blob_name)
+    return render_template('showimage.html', img_url_with_sas_token=img_url_with_sas_token)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run()
 
 
 
